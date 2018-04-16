12b74907ef48cdab486bcfe72924d39c29281f57
horaciocoronel <arielc@gmail.com>
2018-04-15 23:47:28 -0400

    Add mongoDB and mongoose configuration

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('hello÷÷'));
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to mongoDB
+mongoose
+	.connect(db)
+	.then(() => console.log('MongoDB connected'))
+	.catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('hello!'));
 
 const port = process.env.PORT || 5000;
 
