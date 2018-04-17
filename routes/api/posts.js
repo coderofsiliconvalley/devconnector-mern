1d10de3bfad91af2867aaeb77213f8684234e89a
horaciocoronel <arielc@gmail.com>
2018-04-17 13:16:55 -0400

    Add POST api/post/comments/:id route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -122,4 +122,32 @@ router.post('/unlike/:id', passport.authenticate('jwt', { session: false }), (re
 		});
 });
 
+// @route 	POST api/posts/comment/:id
+// @desc 		Add comment to Post
+// @access	Private
+router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const {errors, isValid} = validatePostInput(req.body);
+
+	//Check Validations
+	if(!isValid) {
+		//If any errors, send 400 with errors object
+		return res.status(400).json(errors);
+	}
+
+	Post.findById(req.params.id)
+		.then(post => {
+			const newComment = {
+				text: req.body.text,
+				name: req.body.name,
+				user: req.user.id
+			}
+			// Add to comments array
+			post.comments.unshift(newComment);
+			// Save
+			post.save().then(post => res.json(post));
+		})
+		.catch(err => res.status(404).json({ postnotfound: 'No post found'}));
+});
+
+
 module.exports = router;
