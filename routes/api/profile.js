69134d7a941190751218cb456fd56ccb9904fbe5
horaciocoronel <arielc@gmail.com>
2018-04-17 10:43:49 -0400

    Add DELETE /api/profile/education/:edu_id route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -229,4 +229,25 @@ router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: fal
 		.catch(err => res.status(404).json(err));
 });
 
+// @route 	DELETE api/profile/education/:edu_id
+// @desc 		Delete education from profile
+// @access	Private
+
+router.delete('/educatio/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+
+	Profile.findOne({ user: req.user.id })
+		.then(profile => {
+			// Get remove index
+			const removeIndex = profile.education
+				.map(item => item.id)
+				.indexOf(req.params.exp_id);
+				// Splice out of the array
+				profile.education.splice(removeIndex, 1);
+
+				// save
+				profile.save().then(profile => res.json(profile));
+		})
+		.catch(err => res.status(404).json(err));
+});
+
 module.exports = router;
