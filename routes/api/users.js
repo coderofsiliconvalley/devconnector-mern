595ab61d291b8730eb58b5d3312d7657926712e0
horaciocoronel <arielc@gmail.com>
2018-04-16 22:40:47 -0400

    Add validations for login and register inputs

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,11 @@ const gravatar = require('gravatar');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
+
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
+
 // Load User model
 const User = require('../../models/User');
 
@@ -17,10 +22,17 @@ router.get('/test', (req, res) => res.json({msg: 'Users works'}));
 // @desc 		Register user
 // @access	Public
 router.post('/register', (req, res) => {
+	const { errors, isValid } = validateRegisterInput(req.body);
+// Check validation
+	if(!isValid) {
+		return res.status(400).json(errors);
+	}
+
 	User.findOne({ email: req.body.email })
 		.then(user => {
 			if(user) {
-				return res.status(400).json({email: 'Email already exists'});
+				errors.email = 'Email already exists'
+				return res.status(400).json(errors);
 			} else {
 				const avatar = gravatar.url(req.body.email, {
 					s: '200', // Image size
@@ -51,6 +63,12 @@ router.post('/register', (req, res) => {
 // @desc 		Login user / Returning JWT Token
 // @access	Public
 router.post('/login', (req, res) => {
+	const { errors, isValid } = validateLoginInput(req.body);
+// Check validation
+	if(!isValid) {
+		return res.status(400).json(errors);
+	}
+
 	const email = req.body.email;
 	const password = req.body.password;
 
@@ -58,7 +76,8 @@ router.post('/login', (req, res) => {
 	User.findOne({email})
 		.then(user => {
 			if(!user) {
-				return res.status(404).json({email: 'User not found'});
+				errors.email = 'User not found'
+				return res.status(404).json(errors);
 			}
 
 			// Check password
@@ -80,7 +99,8 @@ router.post('/login', (req, res) => {
 								});
 						});
 					} else {
-						return res.status(400).json({password: 'Password incorrect'});
+						errors.password = 'Password incorrect'
+						return res.status(400).json(errors);
 					}
 				});
 		});
--- validation/login.js
+++ validation/login.js
@@ -1,1 +1,25 @@
-const validator = require('validator');
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateLoginInput(data) {
+	let errors = {};
+
+	// It'll check if it's empty with the function isEmpty created first
+	data.email = !isEmpty(data.email) ? data.email : '';
+	data.password = !isEmpty(data.password) ? data.password : '';
+
+	if(!Validator.isEmail(data.email)) {
+		errors.email = 'Email is invalid'
+	}
+	if(Validator.isEmpty(data.email)) {
+		errors.email = 'Email field is required'
+	}
+	if(Validator.isEmpty(data.password)) {
+		errors.password = 'Password field is required'
+	}
+
+	return {
+		errors,
+		isValid: isEmpty(errors)
+	};
+};
