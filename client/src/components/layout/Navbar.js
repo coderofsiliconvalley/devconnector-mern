5e7bfae8a5fb63e35b38190be314e3e16c15c04c
horaciocoronel <arielc@gmail.com>
2018-04-17 18:13:47 -0400

    Fix links remove .html extension

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -22,10 +22,10 @@ class Navbar extends Component {
 
 						<ul className="navbar-nav ml-auto">
 							<li className="nav-item">
-								<Link className="nav-link" to="register.html">Sign Up</Link>
+								<Link className="nav-link" to="/register">Sign Up</Link>
 							</li>
 							<li className="nav-item">
-								<Link className="nav-link" to="login.html">Login</Link>
+								<Link className="nav-link" to="/login">Login</Link>
 							</li>
 						</ul>
 					</div>
