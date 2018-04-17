a8a892cef3e2428ea462b1779bf8ef6e2b6d98a8
horaciocoronel <arielc@gmail.com>
2018-04-17 12:23:28 -0400

    Add GET api/posts/:id route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -21,7 +21,16 @@ router.get('/', (req, res) => {
 	Post.find()
 		.sort({ date: -1 })
 		.then(posts => res.json(posts))
-		.catch(err => res.status(404).json(err));
+		.catch(err => res.status(404).json({nopostfound: 'No posts found'}));
+});
+
+// @route 	GET api/posts/:id
+// @desc 		Get Posts by id
+// @access	Public
+router.get('/:id', (req, res) => {
+	Post.findById(req.params.id)
+		.then(post => res.json(post))
+		.catch(err => res.status(404).json({nopostfound: 'No post found with that id'}));
 });
 
 
