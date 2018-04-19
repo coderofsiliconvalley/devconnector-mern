b508336c26bafae2721cc93ec4c5232c70a307da
horaciocoronel <arielc@gmail.com>
2018-04-19 11:53:56 -0400

    Fix typo on headers

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const setAuthToken = token => {
 	if(token) {
 		//Apply to every request
-		axios.defaults.header.common['Authorization'] = token;
+		axios.defaults.headers.common['Authorization'] = token;
 	} else {
 	// Delete the auth header if !token
 	delete axios.defaults.headers.common['Authorization'];
