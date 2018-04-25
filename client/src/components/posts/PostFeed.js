15ef29bcfce84302a40da85f9b07c219cbfc7f77
horaciocoronel <arielc@gmail.com>
2018-04-25 11:54:00 -0400

    Remove blank spaces

--- client/src/components/posts/PostFeed.js
+++ client/src/components/posts/PostFeed.js
@@ -3,13 +3,11 @@ import PropTypes from 'prop-types';
 import PostItem from './PostItem';
 
 class PostFeed extends Component {
-
 	render() {
 		const { posts } = this.props;
 
 		return posts.map(post => <PostItem key={post._id} post={post} />)
 	}
-
 }
 
 PostFeed.propTypes = {
