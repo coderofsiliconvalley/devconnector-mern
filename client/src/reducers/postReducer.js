d961b6fd7fa03b3105e5cc8ba2f5343ad163e275
horaciocoronel <arielc@gmail.com>
2018-04-24 23:55:56 -0400

    Set up Post reducers and actions

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+	post: [],
+	post: {},
+	loading: false
+};
+
+export default function(state = initialState, action) {
+	switch (action.type) {
+		default:
+			return state;
+
+	}
+}
