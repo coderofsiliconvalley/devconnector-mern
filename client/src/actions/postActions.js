4124ebf0705f8a318a48ba7c0bd94e78bf3510f9
horaciocoronel <arielc@gmail.com>
2018-04-25 11:53:42 -0400

    Add actions to Like and unlike a post

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -4,7 +4,8 @@ import {
 	ADD_POST,
 	GET_ERRORS,
 	GET_POSTS,
-	POST_LOADING
+	POST_LOADING,
+	DELETE_POST
 } from './types';
 
 // Add post
@@ -27,7 +28,7 @@ export const addPost = postData => dispatch => {
 
 // Get posts
 export const getPosts = () => dispatch => {
-	dispatch(setPostLoading);
+	dispatch(setPostLoading());
 	axios
 		.get('/api/posts')
 		.then(res =>
@@ -43,6 +44,52 @@ export const getPosts = () => dispatch => {
 			})
 		);
 };
+
+// Delete post
+export const deletePost = id => dispatch => {
+	axios
+		.delete(`/api/posts/${id}`)
+		.then(res =>
+			dispatch({
+				type: DELETE_POST,
+				payload: id
+			})
+		)
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+
+// Add Like to post
+export const addLike = id => dispatch => {
+	axios
+		.post(`/api/posts/like/${id}`)
+		.then(res => dispatch(getPosts()))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
+// Remove Like from post
+export const removeLike = id => dispatch => {
+	axios
+		.post(`/api/posts/unlike/${id}`)
+		.then(res => dispatch(getPosts()))
+		.catch(err =>
+			dispatch({
+				type: GET_ERRORS,
+				payload: err.response.data
+			})
+		);
+};
+
 
 // Set loading state
 export const setPostLoading = () => {
