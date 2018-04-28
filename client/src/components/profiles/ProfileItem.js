46699d34c1dee8079e4500e5cdea4915e950e697
horaciocoronel <arielc@gmail.com>
2018-04-28 16:53:16 -0400

    Fix Typo in profile.company

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -15,7 +15,7 @@ class ProfileItem extends Component {
 					</div>
 					<div className="col-lg-6 col-md-4 col-8">
 						<h3>{profile.user.name}</h3>
-						<p>{profile.status} {isEmpty(profile.company) ? null : (<span>at {profile.comapny}</span>)}</p>
+						<p>{profile.status} {isEmpty(profile.company) ? null : (<span>at {profile.company}</span>)}</p>
 						<p>
 							{isEmpty(profile.location) ? null : (<span>{profile.location}</span>)}
 						</p>
